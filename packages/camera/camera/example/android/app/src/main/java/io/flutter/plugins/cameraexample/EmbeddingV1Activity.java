67c0aac497ae47f878ee5168f2de2ade02eeb5fc
alien <alien1900@bk.ru>
2021-11-23 10:40:13 +0700

    integration_test was removed from example app

--- packages/camera/camera/example/android/app/src/main/java/io/flutter/plugins/cameraexample/EmbeddingV1Activity.java
+++ packages/camera/camera/example/android/app/src/main/java/io/flutter/plugins/cameraexample/EmbeddingV1Activity.java
@@ -1,7 +1,6 @@
 package io.flutter.plugins.cameraexample;
 
 import android.os.Bundle;
-import dev.flutter.plugins.integration_test.IntegrationTestPlugin;
 import io.flutter.plugins.camera.CameraPlugin;
 import io.flutter.plugins.pathprovider.PathProviderPlugin;
 import io.flutter.plugins.videoplayer.VideoPlayerPlugin;
@@ -12,8 +11,6 @@ public class EmbeddingV1Activity extends io.flutter.app.FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     CameraPlugin.registerWith(registrarFor("io.flutter.plugins.camera.CameraPlugin"));
-    IntegrationTestPlugin.registerWith(
-        registrarFor("dev.flutter.plugins.integration_test.IntegrationTestPlugin"));
     PathProviderPlugin.registerWith(
         registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
     VideoPlayerPlugin.registerWith(
